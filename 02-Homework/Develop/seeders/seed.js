7fde7fe6d8ff98505ad9d0f10e30e6eef602148a
Zoey Frakes <zfrakes811@gmail.com>
2020-04-02 19:14:28 -0400

    updated seed file

--- 02-Homework/Develop/seeders/seed.js
+++ 02-Homework/Develop/seeders/seed.js
@@ -1,28 +1,31 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/imageperformance", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/transaction", {
   useNewUrlParser: true
 });
 
 const transactionSeed = [
   {
-    description: "small notebook",
-    image: "/icons/icon",
-    rating: 0,
-    date: new Date(Date.now())
-  },
-  {
-    description: "large notebook",
-    image: "/assets/images/2.jpg",
-    rating: 0,
-    date: new Date(Date.now())
+    name:{
+    type: String,
+    trim: true,
+    required: "Enter a name for transaction"
   },
   
+    value: {
+    type: Number,
+    required: "Enter an amount"
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+}
 ];
 
 db.Icon.deleteMany({})
-  .then(() => db.Image.collection.insertMany(iconSeed))
+  .then(() => db.Image.collection.insertMany(transactionSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
