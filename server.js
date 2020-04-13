1e2a016b32f8f0fa3ef2f03e56cbfe23f1e23d9b
Zoey Frakes <zfrakes811@gmail.com>
2020-04-13 17:58:07 -0400

    update server

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://<zfrakes>:<zrf081193>@ds117846.mlab.com:17846/heroku_1d61kk56", {
+mongoose.connect ( "mongodb://heroku_1d61kk56:uotkl008cchojkqp8kmq3ub9q8@ds117846.mlab.com:17846/heroku_1d61kk56", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
