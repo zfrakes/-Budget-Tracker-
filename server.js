913b60d30bfaa5096bff81eba7270fbc5fb1c276
Zoey Frakes <zfrakes811@gmail.com>
2020-04-13 21:37:06 -0400

    update connection

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose
-.connect(process.env.MONGO_URI, {
+.connect(process.env.MONGODB_URI, {
 useUnifiedTopology: true,
 useNewUrlParser: true,
 })
